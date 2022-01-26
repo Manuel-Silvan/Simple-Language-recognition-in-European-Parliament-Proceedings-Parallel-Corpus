b01d971ab2ee513e4f53ad1b62b0df5aa4c9c9bf
Manuel <manuel.silvan091@gmail.com>
2022-01-26 20:21:37 +0100

    ready

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import numpy as np
 import os
 import re
 import random as rd
+import getopt, sys
+
 
 import nltk
 #nltk.download('punkt')
@@ -21,9 +23,45 @@ from bigrama import *
 
 from scipy import sparse
 lista = os.listdir()
-r = re.compile("^europarl-v7(?!.*\.en$).*$")#
+r = re.compile("^europarl-v7.*$")#"^europarl-v7(?!.*\.en$).*$"
 
 ficheros = list(filter(r.findall, lista))
+
+
+
+normalize = 0
+model = 0
+# Remove 1st argument from the
+# list of command line arguments
+argumentList = sys.argv[1:]
+ 
+# Options
+options = "nm:"
+ 
+# Long options
+long_options = ["normalize", "model ="]
+ 
+try:
+    # Parsing argument
+    arguments, values = getopt.getopt(argumentList, options, long_options)
+     
+    # checking each argument
+    for currentArgument, currentValue in arguments:
+ 
+        if currentArgument in ("-n", "--normalize"):
+            normalize = 1
+             
+        
+             
+        elif currentArgument in ("-m", "--model"):
+            model = int(currentValue)
+             
+except getopt.error as err:
+    # output error, and return with an error code
+    print (str(err))
+
+
+
 idiomas = [
 'Aleman',
 'Español',
@@ -32,9 +70,11 @@ idiomas = [
 'Polaco',
 'Ingles'
 ]
-ficheros.append('europarl-v7.es-en.en')
-corpus_train = [[] for i in range(len(idiomas))]
-corpus_val = [[] for i in range(len(idiomas))]
+idiomas = idiomas.sort()
+
+
+corpus_train = [[] for i in range(len(ficheros))]
+corpus_val = [[] for i in range(len(ficheros))]
 
 
 porcentaje_extraccion = 0.01
@@ -84,60 +124,118 @@ corpus_train, corpus_val = normaliza(corpus_train, corpus_val)
 freq100 = [nltk.FreqDist(nltk.word_tokenize(' '.join(i))).most_common(100) for i in corpus_train]
 word100 = [[j[0] for j in i] for i in freq100]
 
+if(normalize):
+    #filtrar frases inutiles
+
+    corpus_train = [[] for i in range(len(ficheros))]
+    corpus_val = [[] for i in range(len(ficheros))]
+
+    for idx,val in enumerate(ficheros):
+        with open(val, encoding="utf8") as var:
+            texto = var.readlines()
+            #Lo nuevo
+
+            Excluidos = list(np.concatenate(word100[:idx] + word100[idx+1:]))
+
+            aux = []
+            i = 0
+            while(len(aux) < frases_entrenamiento):
+                if(nltk.word_tokenize(texto[i]) not in Excluidos):
+                    aux.append(texto[i])
+
+                i+=1
+            texto = aux
+
+
+
+
+
+            for i in range(frases_test):
+                al = rd.randint(0,10)
+                corpus_val[idx].append( texto[al+ 11*i] )
+                corpus_train[idx] = corpus_train[idx] + texto[0 + 11*i:al + 11*i] + texto[al+1 + 11*i :11 + 11*i]
+
+
+
+    corpus_train, corpus_val = normaliza(corpus_train, corpus_val)
+
 
 #Normalizacion y extracción de características TF-IDF
 
+if(model == 0): #TF-IDF scratch
+    vectorizer = tfidf(20)
+    #Concatenamos las frases para dejar una matriz con los idiomas
+    corpus_train_unido = [' '.join(i) for i in corpus_train]
+    TF_IDF = vectorizer.fit_transform(corpus_train_unido)
+
+    #MODELO SIMPLE DE CLASIFICACION
+    #Vectorizamos con el modelo anterior un documento, y hacemos el producto escalar con la columna idioma de la matriz tf_idf, el que de un producto escalar
+    # mas alto gana pues sera el que este más cerca
+    def clasifica(modelo, TF_IDF,X_test):
+        features = modelo.transform(X_test)
+        resultado = np.dot(np.array(TF_IDF),np.array(features).T)
+        h = np.argmax(resultado, axis = 0)
+        h = np.squeeze(np.asarray(h))
+        return h
+
+    corpus_val_unido = np.concatenate(corpus_val)
+
+    y_pred = clasifica(vectorizer,TF_IDF,corpus_val_unido)
+    y_test = np.ones(frases_test*len(ficheros))
+    for i in range(len(ficheros)):
+        y_test[i*frases_test:(i+1)*frases_test] = i
 
 
-vectorizer = tfidf(20)
+elif(model == 1):#bigrama scratch
+    clf = bigrama(2)
+    clf.entrena(corpus_train)
+    corpus_val_unido = np.concatenate(corpus_val)
+    y_pred = clf.clasifica(corpus_val_unido)
+    y_test = np.ones(frases_test*len(ficheros))
+    for i in range(len(ficheros)):
+        y_test[i*frases_test:(i+1)*frases_test] = i
+elif(model == 2):
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    vec = TfidfVectorizer(min_df = 20)
 
-#Concatenamos las frases para dejar una matriz con los idiomas
+    corpus_train_unido = np.concatenate(corpus_train)
+    corpus_val_unido = np.concatenate(corpus_val)
 
-corpus_train_unido = [' '.join(i) for i in corpus_train]
+    X_train= vec.fit_transform(corpus_train_unido)
+    X_test = vec.transform(corpus_val_unido)
+    y_test = np.ones(frases_test*len(ficheros))
+    y_train = np.ones((frases_entrenamiento-frases_test)*len(ficheros))
+    for i in range(len(ficheros)):
+        y_test[i*frases_test:(i+1)*frases_test] = i
+        y_train[i*(frases_entrenamiento-frases_test):(i+1)*(frases_entrenamiento-frases_test)] = i
 
-TF_IDF = vectorizer.fit_transform(corpus_train_unido)
+    from sklearn.tree import DecisionTreeClassifier
 
-#Guardamos la matriz
-
-
-with open('TF_IDF', 'wb') as f:
-    pickle.dump(TF_IDF, f)
-with open('vectorizer', 'wb') as f:
-    pickle.dump(vectorizer, f)
-#Cargamos si necesario
-with open('TF_IDF','rb') as f:
-    TF_IDF = pickle.load(f)
-with open('vectorizer','rb') as f:
-    vectorizer = pickle.load(f)
-
-#MODELO SIMPLE DE CLASIFICACION
+    tree  = DecisionTreeClassifier()
+    tree.fit(X_train,y_train)
+    y_pred = tree.predict(X_test)
 
 
+    #Podemos saber que tokens han sido los más determinantes
+    print('********PALABRAS MAS RELEVANTES PARA DIFERENCIAR ENTRE IDIOMAS********')
+    n_tok = 10
+    index = np.argsort(tree.feature_importances_)
+    tok = [index[-i-1] for i in range(n_tok)]
+    important = [vec.get_feature_names()[i] for i in tok]
 
-
-
-#Vectorizamos con el modelo anterior un documento, y hacemos el producto escalar con la columna idioma de la matriz tf_idf, el que de un producto escalar
-# mas alto gana pues sera el que este más cerca
-def clasifica(modelo, TF_IDF,X_test):
-    features = modelo.transform(X_test)
-    resultado = np.dot(np.array(TF_IDF),np.array(features).T)
-    h = np.argmax(resultado, axis = 0)
-    h = np.squeeze(np.asarray(h))
-    return h
-
-corpus_val_unido = np.concatenate(corpus_val)
-
-y_pred = clasifica(vectorizer,TF_IDF,corpus_val_unido)
-
+    print(important)
 
 
 
-y_test = np.ones(frases_test*len(idiomas))
-for i in range(len(idiomas)):
-    y_test[i*frases_test:(i+1)*frases_test] = i
+else:
+    print('ERROR')
+
+
 
 #Analisis del rendimiento del modelo
-print('********Rendimiento TF-IDF DOT PRODUCT SIN NORMALIZAR********')
+modelos = ["TF-IDF from-scratch", "bigram from-scratch","sklearn TF-IDF"]
+norm = ["sin limpiar corpus","corpus limpio"]
+print("********Rendimiento "+ modelos[model]+" "+norm[normalize]+" ********")
 print('Matriz de confusion')
 print(confusion_matrix(y_test, y_pred))
 # Accuracy
@@ -147,135 +245,23 @@ print('Accuracy: ',accuracy_score(y_test, y_pred))
 print('Recall (por idioma): ',recall_score(y_test, y_pred, average=None))
 # Precision
 print('Precision (por idioma): ',precision_score(y_test, y_pred, average=None))
-#Limpieza de datos
-
-#Recargamos todo pero filtrando antes frases inutiles
-
-corpus_train = [[] for i in range(len(idiomas))]
-corpus_val = [[] for i in range(len(idiomas))]
-
-for idx,val in enumerate(ficheros):
-    with open(val, encoding="utf8") as var:
-        texto = var.readlines()
-        #Lo nuevo
-
-        Excluidos = list(np.concatenate(word100[:idx] + word100[idx+1:]))
-
-        aux = []
-        i = 0
-        while(len(aux) < frases_entrenamiento):
-            if(nltk.word_tokenize(texto[i]) not in Excluidos):
-                aux.append(texto[i])
-
-            i+=1
-        texto = aux
-
-
-
-
-
-        for i in range(frases_test):
-            al = rd.randint(0,10)
-            corpus_val[idx].append( texto[al+ 11*i] )
-            corpus_train[idx] = corpus_train[idx] + texto[0 + 11*i:al + 11*i] + texto[al+1 + 11*i :11 + 11*i]
-
-
-
-corpus_train, corpus_val = normaliza(corpus_train, corpus_val)
-#TF-IDF CORPUS LIMPIO
-
-
-
-vectorizer = tfidf(20)
-
-corpus_train_unido = [' '.join(i) for i in corpus_train]
-TF_IDF = vectorizer.fit_transform(corpus_train_unido)
-corpus_val_unido = np.concatenate(corpus_val)
-y_pred = clasifica(vectorizer,TF_IDF,corpus_val_unido)
-#El y-test usamos el previo puesto que no ha cambiado
-print('********Rendimiento TF-IDF DOT PRODUCT NORMALIZADO********')
-print('Matriz de confusion')
-
-print(confusion_matrix(y_test, y_pred))
-# Accuracy
-
-print('Accuracy: ',accuracy_score(y_test, y_pred))
-# Recall
-
-
-print('Recall (por idioma): ',recall_score(y_test, y_pred, average=None))
-# Precision
-
-print('Precision (por idioma): ',precision_score(y_test, y_pred, average=None))
 
 
 
 
 
 
-clf = bigrama(2)
-clf.entrena(corpus_train)
-corpus_val_unido = np.concatenate(corpus_val)
-y_pred = clf.clasifica(corpus_val_unido)
-y_test = np.ones(frases_test*len(idiomas))
-for i in range(len(idiomas)):
-    y_test[i*frases_test:(i+1)*frases_test] = i
-
-print('********Rendimiento BIGRAMA********')
-print('Matriz de confusion')
-
-print(confusion_matrix(y_test, y_pred))
-# Accuracy
-
-print('Accuracy: ',accuracy_score(y_test, y_pred))
-# Recall
-
-
-print('Recall (por idioma): ',recall_score(y_test, y_pred, average=None))
-# Precision
-
-print('Precision (por idioma): ',precision_score(y_test, y_pred, average=None))
 
 
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-vec = TfidfVectorizer(min_df = 20)
-
-corpus_train_unido = np.concatenate(corpus_train)
-corpus_val_unido = np.concatenate(corpus_val)
-
-X_train= vec.fit_transform(corpus_train_unido)
-X_test = vec.transform(corpus_val_unido)
-y_test = np.ones(frases_test*len(idiomas))
-y_train = np.ones((frases_entrenamiento-frases_test)*len(idiomas))
-for i in range(len(idiomas)):
-    y_test[i*frases_test:(i+1)*frases_test] = i
-    y_train[i*(frases_entrenamiento-frases_test):(i+1)*(frases_entrenamiento-frases_test)] = i
-
-from sklearn.tree import DecisionTreeClassifier
-
-model  = DecisionTreeClassifier()
-model.fit(X_train,y_train)
-y_pred = model.predict(X_test)
-print('Matriz de confusion')
-print('********Rendimiento DECISION TREE basado en TF-IDF (sklearn implementation)********')
-print(confusion_matrix(y_test, y_pred))
-# Accuracy
-
-print('Accuracy: ',accuracy_score(y_test, y_pred))
-# Recall
 
 
-print('Recall (por idioma): ',recall_score(y_test, y_pred, average=None))
-# Precision
 
-print('Precision (por idioma): ',precision_score(y_test, y_pred, average=None))
 
-#Podemos saber que tokens han sido los más determinantes
-print('********PALABRAS MAS RELEVANTES PARA DIFERENCIAR ENTRE IDIOMAS********')
-n_tok = 10
-index = np.argsort(model.feature_importances_)
-tok = [index[-i-1] for i in range(n_tok)]
-important = [vec.get_feature_names()[i] for i in tok]
 
-print(important)
+
+
+
+
+
+
